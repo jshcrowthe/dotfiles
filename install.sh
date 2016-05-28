8281f1a4560ed927792e8c58556500513873ff04
Josh Crowther <jshcrowthe@gmail.com>
2016-05-28 14:23:41 -0600

    Add config scripts

--- install.sh
+++ install.sh
@@ -1,28 +1,40 @@
-for installScript in */install.sh ; do
+# Run Install Scripts
+ for installScript in */install.sh ; do
+   echo "\r\n====================================================";
+   echo "============== Running $installScript ==============";
+   echo "====================================================\r\n";
+   ./$installScript
+ done
+
+# Symlink Dotfiles
+ echo "\r\n====================================================";
+ echo "================ Symlinking Dotfiles ===============";
+ echo "====================================================\r\n";
+ files=`find . -name ".[^.]*" ! -path ./.git ! -path ./.gitignore`;
+ for relativePath in $files ; do
+   dotfile=`echo "$relativePath" | sed 's@.*/@@'`;
+   echo "Linking $dotfile";
+   srcPath=`realpath $relativePath`;
+   destPath=~/$dotfile;
+   if [ ! -e $destPath ]
+   then
+     ln -s $srcPath $destPath;
+   else
+     read -p "$dotfile exists overwrite? [y/n]:" -n 1 -r
+     if [[ $REPLY =~ ^[Yy]$ ]]
+     then
+       rm $destPath;
+       ln -s $srcPath $destPath;
+     fi
+   fi
+ done
+
+# Run Config Scripts
+for configScript in */config.sh ; do
   echo "\r\n====================================================";
-  echo "============== Running $installScript ==============";
+  echo "=============== Running $configScript ==============";
   echo "====================================================\r\n";
-  ./$installScript
+  ./$configScript
 done
 
-echo "\r\n====================================================";
-echo "================ Symlinking Dotfiles ===============";
-echo "====================================================\r\n";
-files=`find . -name ".[^.]*" ! -path ./.git ! -path ./.gitignore`;
-for relativePath in $files ; do
-  dotfile=`echo "$relativePath" | sed 's@.*/@@'`;
-  echo "Linking $dotfile";
-  srcPath=`realpath $relativePath`;
-  destPath=~/$dotfile;
-  if [ ! -e $destPath ]
-  then
-    ln -s $srcPath $destPath;
-  else
-    read -p "$dotfile exists overwrite? [y/n]:" -n 1 -r
-    if [[ $REPLY =~ ^[Yy]$ ]]
-    then
-      rm $destPath;
-      ln -s $srcPath $destPath;
-    fi
-  fi
-done
+source ~/.zshenv
---
+++ sublime/config.sh
@@ -0,0 +1,15 @@
+dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
+
+pushd "$dir" > /dev/null
+
+userDirPath=`realpath ./User`
+
+configPath=~/Library/Application\ Support/Sublime\ Text\ 3/Installed\ Packages;
+pushd "$configPath" > /dev/null;
+
+curl -o "Package Control.sublime-package" https://sublime.wbond.net/Package%20Control.sublime-package
+cp -r $userDirPath ../Packages
+
+popd
+
+popd
