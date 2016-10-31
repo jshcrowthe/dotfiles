e5cc60c1646e14b6c9df86a137c672a5d25cc236
Josh Crowther <jshcrowthe@gmail.com>
2016-10-31 00:37:38 -0600

    Update VSCode Config

--- vsc/config.sh
+++ vsc/config.sh
@@ -4,11 +4,25 @@ pushd "$dir" > /dev/null
 
 packagesPath=`realpath .`
 
-configPath=~/Library/Application\ Support/Code/User/snippets/
-pushd "$configPath" > /dev/null;
+configPath=~/Library/Application\ Support/Code/User/
 
-ln -s $packagesPath/*.json .
+# Script pulled from https://ubuntuforums.org/showthread.php?t=798899
 
-popd
+SRC="$packagesPath/config"
+DEST="$configPath"
+
+mkdir -p $DEST 
+
+## 1 - recreate SRC directory tree as DEST
+find $SRC -type d |while read PATHNAME; do 
+    mkdir -p "$DEST${PATHNAME#$SRC}" ; 
+done
+
+## 2 - create a a symlink under $DEST for each file under $SRC
+find $SRC -type f |while read PATHNAME; do 
+        NEW="$DEST${PATHNAME#$SRC}" ; 
+        echo "$NEW" ; 
+        ln -s "$PATHNAME" "$NEW" ; 
+done
 
 popd
