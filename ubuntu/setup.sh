67f4a6af79630617e622310ffa443a528e741dc5
Josh Crowther <jshcrowthe@gmail.com>
2018-03-04 19:06:26 -0800

    Refactor to have Ubuntu and macOS Setups

---
+++ ubuntu/setup.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# Add missing APT packages
+
+# VS Code
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+
+# Chrome
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+
+# OBS (Open Broadcaster Software)
+sudo add-apt-repository ppa:obsproject/obs-studio
+
+# Update APT Package List
+sudo apt update
+
+# Update Existing Packages
+sudo apt upgrade
+
+# Install Packages
+sudo apt install \
+    code \
+    curl \
+    ffmpeg \
+    git \
+    google-chrome-stable \
+    obs-studio \
+    terminator \
+    vim \
+    zsh
+
+sudo apt install --no-install-recommends yarn
+
+# Install oh-my-zsh (do this in a backround shell to prevent the current shell execution from exiting)
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" & 
+wait
+
+# Make dev dirs
+mkdir -p $HOME/Repos
+mkdir -p $HOME/Sandbox
+mkdir -p $HOME/.bin
+
+# Copy down the dotfiles repo
+git clone https://github.com/jshcrowthe/dotfiles $HOME/Repos/dotfiles
+
+# Get NVM Installed
+git clone https://github.com/lukechilds/zsh-nvm $HOME/.oh-my-zsh/custom/plugins/zsh-nvm
+
+# Symlink the .zshrc
+rm $HOME/.zshrc
+ln -s $HOME/Repos/dotfiles/ubuntu/config/.zshrc $HOME/.zshrc
+
+# Symlink the .gitconfig
+rm $HOME/.gitconfig
+ln -s $HOME/Repos/dotfiles/ubuntu/config/.gitconfig $HOME/.gitconfig
